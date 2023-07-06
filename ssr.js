6c153b277a4c009da4463ea832b4eff591530d39
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-06 21:42:49 +0600

    inject script

--- ssr.js
+++ ssr.js
@@ -11,6 +11,9 @@ let indexHTML = fs.readFileSync(path.resolve( __dirname, './initial-files/index.
 
 let appHTML = renderToString(Page());
 
+const scriptText = fs.readFileSync(path.resolve( __dirname, './script.js'))
+
 indexHTML = indexHTML.replace('<div id="app"></div>', `<div id="app">${appHTML}</div>`)
+indexHTML = indexHTML.replace('<script></script>', `<script>${scriptText}</script>`)
 
 fs.writeFileSync(path.resolve(__dirname, './index.html'), indexHTML)
