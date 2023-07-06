aabab965985f7ad1681be65df07237b5cbe0d98d
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-06 22:06:27 +0600

    inject css

--- ssr.js
+++ ssr.js
@@ -12,8 +12,11 @@ let indexHTML = fs.readFileSync(path.resolve( __dirname, './initial-files/index.
 let appHTML = renderToString(Page());
 
 const scriptText = fs.readFileSync(path.resolve( __dirname, './script.js'))
+const resetCSSText = fs.readFileSync(path.resolve( __dirname, './reset.css'))
+const styleCSSText = fs.readFileSync(path.resolve( __dirname, './styles.css'))
 
 indexHTML = indexHTML.replace('<div id="app"></div>', `<div id="app">${appHTML}</div>`)
 indexHTML = indexHTML.replace('<script></script>', `<script>${scriptText}</script>`)
+indexHTML = indexHTML.replace('<style></style>', `<style>${resetCSSText}${styleCSSText}</style>`)
 
 fs.writeFileSync(path.resolve(__dirname, './index.html'), indexHTML)
